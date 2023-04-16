b3c8a702bfde839747028f9eecbba3203fb023e0
Christopher Casey <chrisacas20@gmail.com>
2023-04-16 14:17:06 -0500

    feature: testing models

---
+++ MNIST_Dataloader.py
@@ -0,0 +1,94 @@
+import numpy as np # linear algebra
+import struct
+from array import array
+import matplotlib.pyplot as plt
+
+class MNIST_Dataloader: 
+    '''
+    http://yann.lecun.com/exdb/mnist/
+    '''
+    def __init__(self): 
+        self.training_labels_path = "./data/train-labels-idx1-ubyte"
+        self.training_images_path = "./data/train-images-idx3-ubyte"
+        self.test_labels_path = "data/t10k-labels-idx1-ubyte"
+        self.test_images_path = "data/t10k-images-idx3-ubyte"
+        
+    '''
+    open label file and open image file with their file paths
+    return 60k label element array (each element is 0-9 numeric label)
+        and 60k image element array (each element 28x28 matrix pixel grid)
+    '''
+    def read_image_labels(self, labels_path, images_path): 
+        labels = []
+        images = []
+
+        # open labels file 
+        #   extract first 8 bytes with file info
+        #   extract remaining bytes to array and turn them to # value from byte
+        with open(labels_path, 'rb') as labels_file:
+            magic, size = struct.unpack(">II", labels_file.read(8)) 
+            labels = array("B", labels_file.read())                 
+
+        # open image file 
+        #   extract first 16 bytes with file info
+        #   extract rest to simple 1D array of len 60k * 784
+        #       each element holds grayscale value
+        with open(images_path, 'rb') as images_file:
+            magic, size, rows, cols = struct.unpack(">IIII", images_file.read(16))     
+            image_data = array("B", images_file.read())
+            
+        # initialize empty 60k len array with entries of a 784 element arrays
+        for i in range(size):
+            images.append([0] * rows * cols)
+
+        # get 784 element segments from stream and put in numpy array
+        # reshape numpy array to be 28x28 matrix representing pixel grid of img
+        # put matrix in previously init'd array
+        for i in range(size): 
+            image = np.array(image_data[i*rows*cols: (i+1)*rows*cols])
+            image = image.reshape(28, 28)
+            images[i][:] = image
+
+        return images, labels
+
+    def get_test_data(self):
+        x_test, y_test = self.read_image_labels(self.test_labels_path, self.test_images_path,)
+        return np.array(x_test), np.array(y_test)
+
+    def get_train_data(self):
+        x_train, y_train = self.read_image_labels(self.training_labels_path, self.training_images_path)
+        return np.array(x_train), np.array(y_train)
+
+    def get_image_shape(self):
+        # TODO: Change this to show either train or test images
+        with open(self.training_images_path, 'rb') as images_file:
+            magic, size, rows, cols = struct.unpack(">IIII", images_file.read(16))
+        return rows, cols
+
+    def show_images(self, rows, cols ): 
+        # TODO: Change this to show either train or test
+        images, labels = self.get_train_data()
+        index = 1
+        for x in zip(images, labels):
+            image = x[0]
+            label = x[1]
+            plt.subplot(rows, cols, index)      # map out the number of suplots  
+            plt.tight_layout(pad=1)             # pad the image for titles
+            plt.imshow(image, cmap="gray")      # set image matrix to grayscale
+            if (labels != ''):
+                plt.title(label, fontsize = 10) # provide plot for title    
+            if index == rows*cols:              # stop adding images when full
+                break    
+            index += 1
+
+        plt.show()
+        
+    def simple_show(self):
+        images, _ = self.get_train_data()
+        for i in range(9):  
+            plt.subplot(330+1 + i)
+            plt.imshow(images[i], cmap=plt.get_cmap('gray'))
+        plt.show()
+
+
+
---
+++ ugly.py
@@ -0,0 +1,213 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+from MNIST_Dataloader import MNIST_Dataloader
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import classification_report
+
+class NeuralNetwok: 
+    def __init__(self, input_size=28*28, output_size=10, h_layers=1, h_neurons_per_layer=256):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.h_layers = h_layers
+        self.h_neurons_per_layer = h_neurons_per_layer
+        self.layers = self.init_layers(input_size, h_neurons_per_layer, output_size)
+
+    # TODO: implement a programmable amount of hidden layer initialization
+    def init_layers(self, input_size, h_neurons_per_layer, output_size):
+        '''
+        Get layer info and develop weight array 
+        initialize random weights for each connection to next layer
+            weight array of output size, in array for every input node 
+        return these weight arrays for each node as layer
+        '''
+        layer1 = np.random.uniform(-.1,.1,size=(input_size, h_neurons_per_layer))\
+            /np.sqrt(input_size * h_neurons_per_layer)
+        
+        layer2 = np.random.uniform(-.1,.1,size=(h_neurons_per_layer, output_size))\
+            /np.sqrt(h_neurons_per_layer * output_size)
+        
+        return [layer1, layer2]
+    
+    def desired_array_out(self, label):
+        '''Turn label into desired output array 
+        input label         5
+        return desire array [0 0 0 0 0 1 0 0 0 0]
+        '''
+        desired_array = np.zeros(self.output_size, np.float32)
+        desired_array[label] = 1
+        
+        return desired_array
+
+#Sigmoid funstion
+def sigmoid(x):
+    return 1/(np.exp(-x)+1)    
+
+#derivative of sigmoid
+def d_sigmoid(x):
+    return (np.exp(-x))/((np.exp(-x)+1)**2)
+
+#Softmax
+def softmax(x):
+    exp_element=np.exp(x-x.max())
+    return exp_element/np.sum(exp_element,axis=0)
+
+#derivative of softmax
+def d_softmax(x):
+    exp_element=np.exp(x-x.max())
+    return exp_element/np.sum(exp_element,axis=0)*(1-exp_element/np.sum(exp_element,axis=0))
+
+    #forward and backward pass
+def mlp_backpropogation(x,y,l1,l2):
+    desired_out = np.zeros((len(y),10), np.float32)
+    desired_out[range(desired_out.shape[0]),y] = 1
+
+    # forward pass
+    ## input layer to hidden layer
+    x_l1=x.dot(l1)
+    x_sigmoid=sigmoid(x_l1)
+    ## hidden layer to output layer
+    x_l2=x_sigmoid.dot(l2)
+    out=softmax(x_l2)
+
+    # backpropogation l2
+    error=2*(out-desired_out)/out.shape[0]*d_softmax(x_l2)
+    update_l2=x_sigmoid.T@error
+
+    
+    # backpropogation l1
+    error=((l2).dot(error.T)).T*d_sigmoid(x_l1)
+    update_l1=x.T@error
+
+    return out,update_l1,update_l2
+
+def predict(x, l1, l2):
+    # forward pass
+    ## input layer to hidden layer
+    x_l1=x.dot(l1)
+    x_sigmoid=sigmoid(x_l1)
+    ## hidden layer to output layer
+    x_l2=x_sigmoid.dot(l2)
+    out=softmax(x_l2)
+
+    return out
+
+def add_gaussian_noise(x, noise_mean, noise_sigma):
+    x_with_noise = copy.deepcopy(x)
+    print(" Adding Guassian Noise")
+    noise_mean = 0.0
+    noise_sigma = 0.05
+    for i in range(0, len(x_with_noise)):
+        for j in range(0, len(x_with_noise[i])):
+            x_with_noise[i][j] =  x_with_noise[i][j] + np.random.normal(noise_mean, noise_sigma)
+            x_with_noise[i][j] = [1.0 if ele > 1.0 else ele for ele in x_with_noise[i][j]]
+    return x_with_noise
+  
+def analytics(y_test, y_pred): 
+    print('\nAccuracy: {:.2f}\n'.format(accuracy_score(y_test, y_pred)))
+
+    print('Micro Precision: {:.2f}'.format(precision_score(y_test, y_pred, average='micro')))
+    print('Micro Recall: {:.2f}'.format(recall_score(y_test, y_pred, average='micro')))
+    print('Micro F1-score: {:.2f}\n'.format(f1_score(y_test, y_pred, average='micro')))
+
+    print('Macro Precision: {:.2f}'.format(precision_score(y_test, y_pred, average='macro')))
+    print('Macro Recall: {:.2f}'.format(recall_score(y_test, y_pred, average='macro')))
+    print('Macro F1-score: {:.2f}\n'.format(f1_score(y_test, y_pred, average='macro')))
+
+    print('Weighted Precision: {:.2f}'.format(precision_score(y_test, y_pred, average='weighted')))
+    print('Weighted Recall: {:.2f}'.format(recall_score(y_test, y_pred, average='weighted')))
+    print('Weighted F1-score: {:.2f}'.format(f1_score(y_test, y_pred, average='weighted')))
+
+    print('\nClassification Report\n')
+    print(classification_report(y_test, y_pred, target_names=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']))
+
+def plot_traintest(plot_title, train_acc, test_acc, epochs):
+    plt.title(plot_title)
+
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+
+    plt.plot(epochs, train_acc, label="Training Accuracy")
+    plt.plot(epochs, test_acc, label="Validation Accuracy")
+
+    plt.legend(loc="upper left")
+    plt.show()
+    plt.clf()
+
+def main():
+    # dataloader = MNIST_Dataloader()
+    # dataloader.show_images(5, 5)
+    # dataloader.simple_show()
+
+    nn = NeuralNetwok()
+    l1 = nn.layers[0]
+    l2 = nn.layers[1]
+    
+    epochs=200
+    lr=0.001
+    batch=30
+
+    y_pred_list = []
+    y_guassian_pred_list = []
+    accuracies, val_accuracies = [], []
+    epochs_list=[]
+
+    dataloader = MNIST_Dataloader()
+    x_train, y_train = dataloader.get_train_data()
+    x_test, y_test = dataloader.get_test_data()
+
+    rand=np.arange(60000)
+    np.random.shuffle(rand)
+
+    for i in range(epochs):
+        sample=np.random.randint(0,x_train.shape[0],size=(batch))
+
+        x=x_train[sample].reshape((-1,28*28))
+        y=y_train[sample]
+        out,update_l1,update_l2=mlp_backpropogation(x,y,l1,l2)
+                  
+        l1=l1-lr*update_l1
+        l2=l2-lr*update_l2
+        
+        # every 10 epochs record accuracy 
+        if(i%10==0):   
+                        
+            # prediction function, get highest probability of classification
+            y_pred_list = np.argmax(predict(x_test.reshape((-1,28*28)), l1, l2), axis=1)
+
+            classification=np.argmax(out,axis=1)
+            training_accuracy=(classification==y).mean()
+            accuracies.append(training_accuracy)
+            
+            val_acc=(y_pred_list==y_test).mean()
+            val_accuracies.append(val_acc.item())
+    
+            epochs_list.append(i)
+            
+            # last iteration test model with gaussian noisse
+            if(i==(epochs-10)):
+                x_test_w_guassian = add_gaussian_noise(x_test, 0.0, 0.15)
+                y_guassian_pred_list = np.argmax(predict(x_test_w_guassian.reshape((-1,28*28)), l1, l2), axis=1)
+                gaussian_acc=(y_guassian_pred_list==y_test).mean()
+                print(f'Epoch {i}: Training Accuracy: {training_accuracy:.3f} | Validation Accuracy w/ Gaussian:{gaussian_acc:.3f}')  
+
+        if(i%10==0): print(f'Epoch {i}: Training Accuracy: {training_accuracy:.3f} | Validation Accuracy:{val_acc:.3f}')
+
+    y_pred = np.array(y_pred_list)
+    confusion = confusion_matrix(y_test, y_pred)
+    print(confusion)
+    
+    # Normal Training and Testing Analytics
+    analytics(y_test, y_pred)
+    plot_title = "Epoch v Accuracy"
+    plot_traintest(plot_title, accuracies, val_accuracies, epochs_list)
+
+    # Normal Training and Testing with Guassian Noise Analytics
+    print("====================================================")
+    print("Analytics of Accuracies when Gaussian noise is added")
+    analytics(y_test, y_guassian_pred_list)
+ 
+  
+if __name__=="__main__":
+    main()
